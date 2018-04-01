a5d022a92709ffb00458e225bd9528d75da1322f
Dhanush Patel <dhanush.patel@ymail.com>
2018-04-01 00:59:30 -0700

    updated  requestor
    updated  requestor

--- data_requestor.py
+++ data_requestor.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import pandas as pd
+
 
 def request_stock_data(stock_ids, start_date='20150101', end_date='20160101'):
 	stocks_request= requests.get("https://www.blackrock.com/tools/hackathon/performance", 
@@ -8,9 +10,14 @@ def request_stock_data(stock_ids, start_date='20150101', end_date='20160101'):
 			 'endDate': '20160101',
 			 'returnsType' :  "DAILY"})
 	stocks_json = stocks_request.json()
-	print(stocks_json)
 	stocks_data = stocks_json['resultMap']['RETURNS']
-	return [extract_date_return(stock_data) for stock_data in stocks_data]
+	stock_to_date_to_price_dict = {stock_data['ticker']: extract_date_return(stock_data) for stock_data in stocks_data}
+	df = pd.DataFrame(stock_to_date_to_price_dict)
+	return df.pct_change().iloc[1:, :]
+
+def compute_daily_percentage_change(col):
+	offset_cols = col[:-1]
+	return (col[1:] - offset_cols)/offset_cols
 
 def extract_date_return(stock_data):
 	return {date: stock_data_for_day['level'] for date, stock_data_for_day in stock_data['returnsMap'].items()}
